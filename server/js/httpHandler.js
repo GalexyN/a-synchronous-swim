de320965c008a9686bbd02967ccb5658c31770e0
Josh Parker <joshBBparker@gmail.com>
2020-12-14 21:09:48 -0700

    Complete implementing swim command

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,6 +15,14 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+
+  if (req.method === 'GET') {
+    var responseMessages = ['left','right','up','down'];
+    var responseMessage = responseMessages[Math.floor(Math.random()*responseMessages.length)];
+
+    res.write(responseMessage);
+  }
+
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
