897e77d2bcb08eb55538f9f1d52e07d40b2dcd2d
Josh Parker <joshBBparker@gmail.com>
2020-12-15 21:46:37 -0700

    Complete background image functionality

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -4,7 +4,7 @@ const headers = require('./cors');
 const multipart = require('./multipartUtils');
 
 // Path for the background image ///////////////////////
-module.exports.backgroundImageFile = path.join('.', 'background.jpg');
+module.exports.backgroundImageFile = path.join('..', 'background.jpg');
 ////////////////////////////////////////////////////////
 
 let messageQueue = null;
@@ -15,19 +15,42 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
-
-  if (req.method === 'GET') {
-    var message = messageQueue.dequeue();
-    if (message) {
-      res.writeHead(200, headers);
-      res.write(message);
-    } else {
-      res.writeHead(204, headers);
+  var respond = function (statusCode, body) {
+    res.writeHead(statusCode, headers);
+    if (body) {
+      res.write(body);
     }
-  } else {
-    res.writeHead(200, headers);
+
+    res.end();
+    next();
   }
 
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
+  if (req.method === 'GET') {
+    if (req.url.length > 1) {
+      var urlPath = req.url;
+      if (urlPath[0] === '/') {
+        urlPath = urlPath.slice(1);
+      }
+
+      var nonRelativePath = path.join('.', urlPath);
+
+      fs.readFile(nonRelativePath, (err, data) => {
+        if (err) {
+          respond(404);
+        } else {
+          respond(200, data);
+        }
+      });
+    } else if (req.url === '/') {
+      var message = messageQueue.dequeue();
+      if (message) {
+        respond(200,message);
+      } else {
+        respond(204);
+      }
+    }
+  } else {
+    respond(200);
+  }
 };
+
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -46,7 +46,7 @@ describe('server responses', () => {
     done();
   });
 
-  xit('should respond with 404 to a GET request for a missing background image', (done) => {
+  it('should respond with 404 to a GET request for a missing background image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
     let {req, res} = server.mock(httpHandler.backgroundImageFile, 'GET');
 
@@ -57,9 +57,17 @@ describe('server responses', () => {
     });
   });
 
-  xit('should respond with 200 to a GET request for a present background image', (done) => {
+  it('should respond with 200 to a GET request for a present background image', (done) => {
     // write your test here
-    done();
+    httpHandler.backgroundImageFile = path.join('.', 'spec', 'water-lg.jpg');
+
+    let {req, res} = server.mock(httpHandler.backgroundImageFile, 'GET');
+
+    httpHandler.router(req, res, () => {
+      expect(res._responseCode).to.equal(200);
+      expect(res._ended).to.equal(true);
+      done();
+    })
   });
 
   var postTestFile = path.join('.', 'spec', 'water-lg.jpg');
